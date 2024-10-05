12f9f52b387e0d78e06d4506523b3ecaba4db270
Luka Stošić <158442187+Stosic7@users.noreply.github.com>
2024-10-05 15:39:13 +0200

    Update Poklon.h

--- Blanketi/I Kolokvijum 2024/Poklon.h
+++ Blanketi/I Kolokvijum 2024/Poklon.h
@@ -1,27 +1,27 @@
 #pragma once
-#include <iostream>
-#include <fstream>
+#include <iostream> // uvek na pocetak
+#include <fstream> // uvek na pocetak
 
-using namespace std;
+using namespace std; // kontejnerska klasa, koristi je, lakse je
 
-class Poklon
+class Poklon // javna klasa (apstraktna)
 {
 
-protected:
+protected: // zasticeno pravo pristupa
 
 	double cena;
 	int brojMasnica;
 
-public:
+public: // javno pravo pristupa
 
-	Poklon();
-	Poklon(double _cena, int _brojMasnica);
-	virtual ~Poklon();
-	Poklon(const Poklon& og);
+	Poklon(); // default konstruktor
+	Poklon(double _cena, int _brojMasnica); //  custom konstruktor
+	virtual ~Poklon(); // destruktor, mora biti virtuelni jer je klasa apstraktna, znaci treba da se nasledi
+	Poklon(const Poklon& og); // copy konstruktor, tako ga pamti, uvek ga definises ovako, implementacija uvek ista
 
-	virtual void prikazi(ostream& izlaz) const;
-	friend ostream& operator<<(ostream& izlaz, const Poklon& p);
+	virtual void prikazi(ostream& izlaz) const; // virtualna funkcija za prikaz (output)
+	friend ostream& operator<<(ostream& izlaz, const Poklon& p); // operator <<, uvek se dodaje
 
-	virtual int vratiBrojMasnica() const = 0;
+	virtual int vratiBrojMasnica() const = 0; // virtuelna apstraktna funkcija, implementira se u naslednicima ove klase
 
 };
