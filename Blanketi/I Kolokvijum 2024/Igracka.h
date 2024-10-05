7ffef8eef13c9fffd4d5939332cd680f21661054
Luka Stošić <158442187+Stosic7@users.noreply.github.com>
2024-10-05 15:48:43 +0200

    Update Igracka.h

--- Blanketi/I Kolokvijum 2024/Igracka.h
+++ Blanketi/I Kolokvijum 2024/Igracka.h
@@ -2,23 +2,23 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <fstream>
-#include "Poklon.h"
+#include "Poklon.h" // include za poklon, jer ova klasa treba da ga nasledi
 
-using namespace std;
+using namespace std; // kontejnerska klasa
 
-class Igracka : public Poklon
+class Igracka : public Poklon // nasledjuje Poklon
 {
-private:
-	char* tipMaterijala;
+private: // private pravo pristupa
+	char* tipMaterijala; // dinamicka promenljiva
 
 public:
-	Igracka();
-	Igracka(double _cena, int _brojMasnica, const char* _tipMaterijala);
-	Igracka(const Igracka& og);
-	~Igracka();
+	Igracka(); // default konstruktor
+	Igracka(double _cena, int _brojMasnica, const char* _tipMaterijala); // ovaj constructor mora da ima parametre koje ima poklon + svoje parametre
+	Igracka(const Igracka& og); // copy constructor
+	~Igracka(); // destruktor
 
-	void prikazi(ostream& izlaz) const;
+	void prikazi(ostream& izlaz) const; // nema virtual, jer ce samo da overriduje iz nasladjene klase
 
-	int vratiBrojMasnica() const;
+	int vratiBrojMasnica() const; // ovde implementiramo ovu funkciju
 
 };
