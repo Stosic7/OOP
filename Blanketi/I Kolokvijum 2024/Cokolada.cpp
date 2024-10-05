f1e358f29fc71e9b511b9ff83c34469afce956c4
Luka Stošić <158442187+Stosic7@users.noreply.github.com>
2024-10-05 16:08:00 +0200

    Update Cokolada.cpp

--- Blanketi/I Kolokvijum 2024/Cokolada.cpp
+++ Blanketi/I Kolokvijum 2024/Cokolada.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
 #include <fstream>
-#include "Cokolada.h"
+#include "Cokolada.h" // include za cokoladu
 
 using namespace std;
 
-Cokolada::Cokolada() : Poklon()
+Cokolada::Cokolada() : Poklon() // default konstruktor koji nasledjuje Poklon()
 {
 	procenatKakaa = 0;
 }
 
-Cokolada::Cokolada(int _cena, double _brojMasnica, int _procenatKakaa) : Poklon(_cena, _brojMasnica)
+Cokolada::Cokolada(int _cena, double _brojMasnica, int _procenatKakaa) : Poklon(_cena, _brojMasnica) // custom konstruktor koji nasledjuje Poklon(_cena, _brojMasnica)
 {
 
 	procenatKakaa = _procenatKakaa;
 
 }
 
-Cokolada::Cokolada(const Cokolada& og) : Poklon(og)
+Cokolada::Cokolada(const Cokolada& og) : Poklon(og) // copy konstruktor koji nasledjuje
 {
-	procenatKakaa = og.procenatKakaa;
+	procenatKakaa = og.procenatKakaa; // kao u osnovnoj klasi
 }
 
-void Cokolada::prikazi(ostream& izlaz) const
+void Cokolada::prikazi(ostream& izlaz) const // overirde za prikazi
 {
-	Poklon::prikazi(izlaz);
-	izlaz << "Procenat kakaa: " << procenatKakaa << endl;
+	Poklon::prikazi(izlaz); // over se "prepisuje" iz osnovne klase
+	izlaz << "Procenat kakaa: " << procenatKakaa << endl; // ovde se doradjuje za ccokoladu i njene parametre
 	izlaz << endl;
 }
 
 int Cokolada::vratiBrojMasnica() const
 {
-	return 2 * brojMasnica;
+	return 2 * brojMasnica; // ovo je uslov zadatka
 }
