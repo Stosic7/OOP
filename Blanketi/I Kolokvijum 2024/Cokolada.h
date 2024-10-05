d4fdfd7b8b7060c4489b83ebc68a2b8f27f80cc7
Luka Stošić <158442187+Stosic7@users.noreply.github.com>
2024-10-05 16:02:27 +0200

    Update Cokolada.h

--- Blanketi/I Kolokvijum 2024/Cokolada.h
+++ Blanketi/I Kolokvijum 2024/Cokolada.h
@@ -1,22 +1,25 @@
 #pragma once
 #include <iostream>
 #include <fstream>
-#include "Poklon.h"
+#include "Poklon.h" // include za Pokloj
 
 using namespace std;
 
-class Cokolada : public Poklon
+class Cokolada : public Poklon // nasledjuje apstraktnu klasu Poklon
 {
-private:
+private: // private pravo pristupa
 	int procenatKakaa;
 
-public:
-	Cokolada();
-	Cokolada(int _cena, double _brojMasnica, int _procenatKakaa);
-	Cokolada(const Cokolada& og);
-	~Cokolada() {}
+public: // public pravo pristupa
+	Cokolada(); // default konstruktor
+	Cokolada(int _cena, double _brojMasnica, int _procenatKakaa); // custom konstruktor koji ima parametre iz osnovne klase Poklon + svoje parametre
+	Cokolada(const Cokolada& og); // copy konstruktor
+	~Cokolada() {} // desturktor
 
-	void prikazi(ostream& izlaz) const;
+	void prikazi(ostream& izlaz) const; // overriduje funkciju iz osnovne klase Poklon
 
-	int vratiBrojMasnica() const;
+	int vratiBrojMasnica() const; // ovde se implementira
+
+	// ovde ne treba operator <<, jer se operatori NE NASLEDJUJU. Koristimo funkciju prikazi koja ce biti overirdovana (updatovana) u zavisnosti koji objekat se pravi, pa ce ona kao takva
+	// pozvati biti pozvana u operatoru << koji je definisan u Poklon.cpp
 };
